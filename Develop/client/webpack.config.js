a3d4951250ad1f95171626f44e8e0f5463b72319
anuD <adilbagi22@gmail.com>
2023-09-05 17:37:19 -0700

    Added css loaders and babel to webpack

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -4,8 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+// Add CSS loaders and babel to webpack.
 module.exports = () => {
   return {
     mode: 'development',
@@ -18,12 +17,28 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'Client Server',
+        template: './index.html',
+      })
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
       ],
     },
   };
